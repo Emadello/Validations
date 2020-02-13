f0fe6fae5e1ef73b4412b6f04240328dc2ba0f28
Emad Elshafie <emad_moh@outlook.com>
2020-02-13 22:50:32 +0200

    added validate.php

---
+++ validate.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Emadello\Validations;
+
+class validate {
+
+  public function __construct() {
+  }
+
+  public function isNumeric($id) {
+    if (!\is_numeric($id)) return false;
+    return true;
+  }
+
+  public function isValidName($name) {
+    if (!$name || $name === '' || strlen($name) < 3) return false;
+    return true;
+  }
+
+  public function isValidEmail($email) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) return false;
+    return true;
+  }
+
+  public function isValidPhone($phone) {
+    if (!$phone || $phone === '' || strlen($phone) < 10) return false;
+    return true;
+  }
+
+  public function isValidTextLength($text) {
+    if (!$text || $text === '' || strlen($text) < 4) return false;
+    return true;
+  }
+
+  public function passwordMismatch($password, $cpassword) {
+
+    if ($password !== $cpassword) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+}
+
+?>
